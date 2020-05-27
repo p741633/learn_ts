a8c84b1cf9890892aaa29ea0fde38cb996f89a69
PiriyaV <piriya_v@gpo.or.th>
2020-05-27 17:56:35 +0700

    Day 2

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
-/*
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]; // tuple
-} = {
-    name: 'Piriya',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author'],
-};
-*/
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Piriya',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-// person.role.push('admin');
-// person.role[1] = 10;
-// person.role = [0, 'admin', 'user'];
-var favoriteActiveities;
-favoriteActiveities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR because map not for string
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Fo';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
+var result = generateError('An error occurred!', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,45 +1,16 @@
-/*
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]; // tuple
-} = {
-    name: 'Piriya',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author'],
-};
-*/
+let userInput: unknown;
+let userName: string;
 
-enum Role {
-    ADMIN,
-    READ_ONLY,
-    AUTHOR,
+userInput = 5;
+userInput = 'Fo';
+
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
 
-const person = {
-    name: 'Piriya',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN,
-};
-
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActiveities: string[];
-favoriteActiveities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR because map not for string
+function generateError(message: string, code: number) {
+    throw { message: message, errorCode: code };
 }
 
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+const result = generateError('An error occurred!', 500);
+console.log(result);
