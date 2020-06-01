912b4c1f21eeddff4f980179060903924dcc9f23
PiriyaV <piriya_v@gpo.or.th>
2020-06-01 17:31:28 +0700

    Day 4

--- src/app.ts
+++ src/app.ts
@@ -20,12 +20,12 @@ age = 29;
 
 const add = (a: number, b: number = 1) => a + b;
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+const printOutput: (a: number | string) => void = (output) => console.log(output);
 
 const button = document.querySelector('button');
 
 if (button) {
-  button.addEventListener('click', event => console.log(event));
+    button.addEventListener('click', (event) => console.log(event));
 }
 
 printOutput(add(5));
@@ -36,8 +36,8 @@ const activeHobbies = ['Hiking'];
 activeHobbies.push(...hobbies);
 
 const person = {
-  name: 'Max',
-  age: 30
+    name: 'Max',
+    age: 30,
 };
 
 const copiedPerson = { ...person };
